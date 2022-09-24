df59f08749636eca0cc854383bda3ae94db809df
taken4grant3d <98580974+taken4grant3d@users.noreply.github.com>
2022-09-24 12:33:10 -0400

    test

--- solution.py
+++ solution.py
@@ -15,7 +15,7 @@ def webServer(port=13331):
   while True:
     #Establish the connection
     
-    print('Ready to serve...')
+    #print('Ready to serve...')
     connectionSocket, addr = serverSocket.accept()
     
     try:
